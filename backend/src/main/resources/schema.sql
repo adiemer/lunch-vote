c6685ce1c7611131a3af3b11c6094d23d00b11d8
Andrew Diemer <adiemer@gmail.com>
2026-03-13 22:44:48 -0400

    Adding the schedule table.

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,9 +1,17 @@
 -- Drop the table if it exists to ensure a fresh start
 DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS schedules;
 
 CREATE TABLE IF NOT EXISTS restaurants (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255),
     label VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS schedules (
+    id SERIAL PRIMARY KEY,
+    lunch_date DATE NOT NULL,
+    restaurant_id INTEGER REFERENCES restaurants(id),
+    label VARCHAR(255)
 );
